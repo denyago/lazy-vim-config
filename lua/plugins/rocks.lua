73b8d4c0f790a52cef106f1da4e398f429a15522
Denys Yahofarov <denyago@gmail.com>
2025-12-21 00:52:26 +0100

    Disable luarocks, try 2

--- lua/plugins/rocks.lua
+++ lua/plugins/rocks.lua
@@ -4,8 +4,7 @@ return {
     opts = function(_, opts)
       opts.rocks = opts.rocks or {}
       opts.rocks.enabled = false
-      -- alternatively:
-      -- opts.rocks.hererocks = false
+      opts.rocks.hererocks = false
     end,
   },
 }
