75228441bafd0f502902f752750037567e64b49b
Denys Yahofarov <denyago@gmail.com>
2025-12-21 00:38:49 +0100

    Remane mason plugins

--- lua/plugins/nix-first.lua
+++ lua/plugins/nix-first.lua
@@ -1,23 +1,16 @@
 return {
-  -- Stop Mason from managing PATH / auto-installs (we install tools via Nix)
+  -- Nix-first: don't let Mason manage installs or PATH
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = {}
-      opts.PATH = "skip" -- don't prepend mason/bin to PATH
+      opts.PATH = "skip"
     end,
   },
   {
-    "williamboman/mason-lspconfig.nvim",
+    "mason-org/mason-lspconfig.nvim",
     opts = { automatic_installation = false },
   },
-  { "WhoIsSethDaniel/mason-tool-installer.nvim", enabled = false },
 
-  -- Remove Lazy's luarocks/hererocks warnings (you don't need rocks)
-  {
-    "folke/lazy.nvim",
-    opts = function(_, opts)
-      opts.rocks = { enabled = false }
-    end,
-  },
+  { "WhoIsSethDaniel/mason-tool-installer.nvim", enabled = false },
 }
