71540badf0d90688435901c55d809d4507c9c80e
Denys Yahofarov <denyago@gmail.com>
2025-12-21 10:42:33 +0100

    Add json formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,7 +3,15 @@ return {
     "stevearc/conform.nvim",
     opts = function(_, opts)
       opts.formatters_by_ft = opts.formatters_by_ft or {}
+
+      -- Disable fish formatter entirely
       opts.formatters_by_ft.fish = nil
+
+      -- JSON / JSONC formatter
+      opts.formatters_by_ft.json = { "prettier" }
+      opts.formatters_by_ft.jsonc = { "prettier" }
+
+      return opts
     end,
   },
 }
